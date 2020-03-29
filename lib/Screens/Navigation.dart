b5aa18a75a716b5cc56e8fb86dffab34f267b4ea
Art Young <art.tyoung@gmail.com>
2020-03-29 04:07:16 -0500

    Fixed AppBar

--- lib/Screens/Navigation.dart
+++ lib/Screens/Navigation.dart
@@ -1,5 +1,6 @@
 import 'package:drivingratingapp/Screens/Leaderboard.dart';
 import 'package:drivingratingapp/Screens/Dashboard.dart';
+import 'package:drivingratingapp/Screens/Login.dart';
 import 'package:flutter/material.dart';
 import 'package:drivingratingapp/Screens/Map.dart';
 
@@ -24,11 +25,85 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          titleSpacing: 100.0,
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                icon: Icon(
+                  Icons.assignment,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+                tooltip: 'Goals',
+              );
+            },
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.exit_to_app),
+              tooltip: 'Logout',
+              onPressed: () => Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => Login())),
+            ),
+          ],
+          centerTitle: true,
           backgroundColor: Color.fromRGBO(25, 22, 55, 1),
           title: Text(
             'SafeSpeed',
             style: TextStyle(fontFamily: 'Calibri'),
+          ),
+        ),
+        drawer: Theme(
+          data: Theme.of(context).copyWith(
+            canvasColor: Color.fromRGBO(35, 35, 35, 1.0),
+          ),
+          child: Drawer(
+            child: ListView(
+              children: <Widget>[
+                DrawerHeader(
+                  child: Container(
+                    child: Center(
+                      child: Text(
+                        '—Goals—',
+                        style: TextStyle(
+                          fontSize: 50.0,
+                          color: Color.fromRGBO(0, 230, 255, 100),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    '1. Make softer turns.',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromRGBO(255, 251, 230, 1.0),
+                    ),
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    '2. Switch between drive and reverse when parking.',
+                    style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromRGBO(255, 251, 230, 1.0)),
+                  ),
+                ),
+                ListTile(
+                  title: Text(
+                    '3. Reach the 90th percentile.',
+                    style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromRGBO(255, 251, 230, 1.0)),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
         body: _pages[_selectedTab],
