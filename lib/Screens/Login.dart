785ef934ad19acf7f6468a2e4a83592f4c021581
Andrewuwu <maosteram@gmail.com>
2020-03-28 22:33:40 -0500

    Login still looks bad lol

--- lib/Screens/Login.dart
+++ lib/Screens/Login.dart
@@ -1,3 +1,4 @@
+import 'package:drivingratingapp/Screens/Navigator.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -6,8 +7,86 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final TextEditingController _textController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [
+              .2,
+              .5,
+              .8
+            ],
+                colors: [
+              Colors.lightBlueAccent,
+              Colors.lightBlue,
+              Colors.blue
+            ])),
+        child: Container(
+          height: double.infinity,
+          child: SingleChildScrollView(
+            physics: AlwaysScrollableScrollPhysics(),
+            padding: EdgeInsets.symmetric(
+              vertical: 40.0,
+              horizontal: 60.0,
+            ),
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 30,
+                ),
+
+                Image(
+                  image: AssetImage('assets/safeSpeed.png'),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+
+                Container(
+                  height: 35,
+                  color: Colors.white70,
+                  child: TextField(
+                    controller: _textController,
+                    decoration: InputDecoration(labelText: ('  Username')),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 5,
+                ),
+
+                //username
+                SizedBox(
+                  width: 375,
+                  child: Container(
+                    height: 35,
+                    width: 300,
+                    color: Colors.white70,
+                    child: TextField(
+                      controller: _textController,
+                      decoration: InputDecoration(labelText: ('  Password')),
+                      obscureText: true,
+                    ),
+                  ),
+                ), //password
+                SizedBox(
+                  height: 30,
+                ),
+                RaisedButton(
+                  child: Text('Login'),
+                  onPressed: () => Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MyHomePage())),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
