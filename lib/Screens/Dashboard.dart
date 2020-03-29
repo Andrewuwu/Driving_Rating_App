0aefbbc580a2003d3f4fa7dcd652e67c0e00acca
Art Young <art.tyoung@gmail.com>
2020-03-28 23:50:26 -0500

    Finished Foundation of Dashboard

--- lib/Screens/Dashboard.dart
+++ lib/Screens/Dashboard.dart
@@ -8,9 +8,34 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  final List<String> goals = [
+    'Make softer turns.',
+    'Switch to reverse less when parking.',
+    'Reach the 90th percentile.'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            DrawerHeader(
+              child: Container(
+                child: Text(
+                  '—Goals—',
+                  style: TextStyle(fontSize: 50.0),
+                ),
+              ),
+            ),
+            Container(
+              child: ListView.builder(
+                itemCount: goals.length,
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Column(
         children: <Widget>[
           ScoreDisplay(),
