2adc66f59ffaf7886ca58c3e8cb6d9fcb73a46e8
Andrewuwu <maosteram@gmail.com>
2020-03-28 19:57:09 -0500

    Completed Navigator between Screens

--- lib/Screens/Leaderboard.dart
+++ lib/Screens/Leaderboard.dart
@@ -6,8 +6,56 @@ class Leaderboard extends StatefulWidget {
 }
 
 class _LeaderboardState extends State<Leaderboard> {
+  final List<String> _names = <String>[
+    'Andrew',
+    'John' 'Bob' 'Cameron' 'Bill' 'Vincent' 'Samuel' 'Art' 'Lucas'
+  ];
+  final List<String> _points = <String>[
+    '10000',
+    '1234' '235' '195' '187' '124' '97' '64' '0'
+  ];
   @override
   Widget build(BuildContext context) {
-    return Text('Leaderboard');
+    return Container(
+      decoration: BoxDecoration(shape: BoxShape.circle),
+      padding: EdgeInsets.all(10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          RaisedButton(
+            child: Text('Weekly'),
+            onPressed: () {
+              _displayRanking();
+            },
+          ),
+          RaisedButton(
+            child: Text('Monthly'),
+            onPressed: () {},
+          ),
+          RaisedButton(
+            child: Text('All Time'),
+            onPressed: () {
+              _displayRanking();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _displayRanking() {
+    return ListView(
+      children: <Widget>[_buildRanking()],
+    );
+  }
+
+  Widget _buildRanking() {
+    for (int i = 0; i < _names.length; i++) {
+      return ListTile(
+        leading: Icon(Icons.account_circle),
+        title: Text(_names[i]),
+        subtitle: Text('points' + _points[i]),
+      );
+    }
   }
 }
